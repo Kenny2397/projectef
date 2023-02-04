ce56c5d3cb57a9cb641ec200b6efda192cf4e702
Kenny Luque <kluque@geovictoria.com>
2023-02-04 10:15:48 -0500

    using DataAnnotations

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,9 +1,14 @@
 namespace projectef.Models;
 
+using System.ComponentModel.DataAnnotations;
 public class Category
 {
+  [Key]
   public Guid CategoryId {get; set;}
+  [Required]
+  [MaxLength(150)]
   public string Name {get; set;}
+  [Required]
   public string Description {get; set;}
   public virtual ICollection<Task> Tasks {get; set;}
 }
--- Models/Task.cs
+++ Models/Task.cs
@@ -1,14 +1,31 @@
 namespace projectef.Models;
 
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 public class Task 
 {
+  [Key]
   public Guid TaskId {get; set;}
+
+  [ForeignKey("CategoryId")]
   public Guid CategoryId {get; set;}
+
+  [Required, MinLength(3), MaxLength(50)]
   public string Title {get; set;}
+  
+  [Required, MinLength(3), MaxLength(250)]
   public string Description {get; set;}
+  
   public Priority PriorityTask {get; set;}
+  
+  [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
   public DateTime CreatedAt {get;set;}
+
+  public DateTime UpdatedAt {get; set;}
   public virtual Category Category {get; set;}
+
+  [NotMapped]
+  public string Resume {get; set;}
 }
 
 public enum Priority 
