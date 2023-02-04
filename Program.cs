2759fbd5994da899e2c5bc273368e30347716267
Kenny Luque <kluque@geovictoria.com>
2023-02-04 10:35:17 -0500

    Connecting to DB in  memory

--- Program.cs
+++ Program.cs
@@ -1,6 +1,18 @@
+using projectef;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TasksContext>(p => p.UseInMemoryDatabase("TasksDB"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconnection", async ([FromServices] TasksContext dbcontext) => {
+  dbcontext.Database.EnsureCreated();
+  return Results.Ok("Database connection: " + dbcontext.Database.IsInMemory());
+} );  
 
 app.Run();
